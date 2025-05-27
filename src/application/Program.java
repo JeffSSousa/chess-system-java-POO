fd03ba3ce519e179ce2191b26325c8e53bc9b77f
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-05-27 11:25:57 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	public static void main(String[]args) {
 		
 	System.out.println("Created Project");	
+	
+	Position pos = new Position(2, 3);
+	
+	System.out.println(pos);
 		
 	}
 }
